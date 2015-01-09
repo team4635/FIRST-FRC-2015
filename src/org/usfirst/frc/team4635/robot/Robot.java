2b44f5afde4a58777d282ed2901f9c769dfdceeb
Antonio Torres <thetonio96@gmail.com>
2015-01-09 14:07:54 -0600

    Buttons now work

--- src/org/usfirst/frc/team4635/robot/Robot.java
+++ src/org/usfirst/frc/team4635/robot/Robot.java
@@ -2,9 +2,14 @@ package org.usfirst.frc.team4635.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+import edu.wpi.first.wpilibj.command.Command;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -14,8 +19,12 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  * directory.
  */
 public class Robot extends IterativeRobot {
+	Command test2;
 	RobotDrive myRobot;
 	Joystick stick;
+	Relay mVentana;
+	Button btn_a;
+	Button btn_b;
 
 	
 	int autoLoopCounter;
@@ -25,14 +34,18 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-    	myRobot = new RobotDrive(0,1,2,3);
+   	    myRobot = new RobotDrive(0,1,2,3);
     	stick = new Joystick(0);
+    	mVentana = new Relay(0);
+    	btn_a = new JoystickButton(stick, 1);
+    	btn_b = new JoystickButton(stick, 2);
+
     }
-    
+    	
     /**
      * This function is run once each time the robot enters autonomous mode
      */
-    public void autonomousInit() {
+    public void aupruebatonomousInit() {
     	autoLoopCounter = 0;
     }
 
@@ -40,26 +53,33 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-    	if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
-		{
-			myRobot.drive(-0.5, 0.0); 	// drive forwards half speed
-			autoLoopCounter++;
-			} else {
-			myRobot.drive(0.0, 0.0); 	// stop robot
-		}
+ 	   
     }
     
     /**
      * This function is called once each time the robot enters tele-operated mode
      */
     public void teleopInit(){
+   	
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        myRobot.arcadeDrive(stick);
+    	myRobot.arcadeDrive(stick); 
+    	if(btn_a.get() || btn_b.get()){
+    		mVentana.set(Relay.Value.kOn);
+    		if(btn_a.get()){
+    			mVentana.set(Relay.Value.kForward);
+    		}else if(btn_b.get()){
+    			mVentana.set(Relay.Value.kReverse);
+    		}
+    	}
+    	else{
+    	   mVentana.set(Relay.Value.kOff);
+       }
+  	  
     }
     
     /**
@@ -67,6 +87,8 @@ public class Robot extends IterativeRobot {
      */
     public void testPeriodic() {
     	LiveWindow.run();
+       
+       
     }
-    
+  
 }
